277fac6c9dedd05dbb8f53335df0c5fb70f2eb43
Ankit Mittal <mittalanku123@gmail.com>
2025-01-10 01:22:32 +0530

    Update Loading and viewing dataset.R

---
+++ Loading and viewing dataset.R
@@ -0,0 +1,9 @@
+library(dplyr)
+library(ggplot2)
+library(readr)
+
+# Loading the Superstore dataset
+superstore <- read_csv("C:/Users/mitta/OneDrive/Desktop/Superstore.csv")
+
+# Viewing the dataset
+View(superstore)
